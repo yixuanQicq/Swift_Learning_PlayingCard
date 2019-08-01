56a2a880dd5e9291a9655e3f716e563d92cbf756
yixuanqi <yixuan99316@gmail.com>
2019-07-31 18:36:58 -0700

    added playing card

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
     }
 
 
